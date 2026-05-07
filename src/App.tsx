affa15bd74f71e3d222ae5c4d71f24c4721e627f
[dyad] <viaasistemas@gmail.com>
2026-05-07 15:54:18 -0300

    [dyad] Garantindo que a página role para o topo ao mudar de rota - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SessionContextProvider } from "./components/SessionContextProvider";
+import ScrollToTop from "./components/ScrollToTop";
 import Index from "./pages/Index";
 import Editais from "./pages/Editais";
 import EditaisPNAB from "./pages/EditaisPNAB";
@@ -28,6 +29,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/editais" element={<Editais />} />
