bd27be898b6ea1a309709fd6004c78ed541ef7e4
[dyad] <viaasistemas@gmail.com>
2026-06-28 17:03:28 -0300

    Reverted all changes back to version eb7392bc47b64dc20be483a86ee382d814ac3478

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -43,7 +43,7 @@ const Index = () => {
       <Navbar />
 
       {/* Hero Section */}
-      <section className="relative pt-12 md:pt-20 pb-20 overflow-hidden min-h-[85vh] flex items-start justify-center bg-slate-50/30">
+      <section className="relative pt-24 md:pt-36 pb-20 overflow-hidden min-h-[85vh] flex items-start justify-center bg-slate-50/30">
         {/* Background Image */}
         <div className="absolute inset-0 z-0 opacity-90">
           <img 
@@ -53,7 +53,7 @@ const Index = () => {
           />
         </div>
 
-        <div className="container mx-auto px-4 relative z-10 text-center max-w-5xl -mt-12 md:-mt-24">
+        <div className="container mx-auto px-4 relative z-10 text-center max-w-5xl mt-4 md:mt-8">
           <h1 className="text-[2.2rem] md:text-7xl font-extrabold mb-4 tracking-tight leading-[1.15] text-[#3c64c1]">
             Transforme sua ideia em <br />
             <span className="text-[#f9b233] italic">Realidade</span>
